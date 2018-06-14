e6a7948cc6e59171e3b1f6bc163c935c9e5c3aea
Egor Safronov <heysafronov@gmail.com>
2018-06-14 20:09:53 +0300

    edit webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    mode: 'development',
+    mode: 'production',
     module: {
         rules: [
             {
