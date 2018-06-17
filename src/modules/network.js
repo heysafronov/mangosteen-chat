63db117078fd560327745476633598d6a0212cf2
Egor Safronov <heysafronov@gmail.com>
2018-06-17 22:11:50 +0300

    edited settings network

--- src/modules/network.js
+++ src/modules/network.js
@@ -9,7 +9,7 @@ class Network {
         return fetch(this.url)
             .then(response => {
                 if (response.status !== 200) {
-                    console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+                    console.error(`Looks like there was a problem. Status Code: ${response.status}`);
                     return;
                 }
                 return response.json();
