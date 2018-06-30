0bffc706bd1725a65ec265fa047551f6dfa7718d
Egor Safronov <heysafronov@gmail.com>
2018-06-30 13:01:42 +0300

    edited webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,38 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    devtool: 'source-map',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[chunkhash].js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {loader: 'babel-loader'}
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {loader: MiniCssExtractPlugin.loader},
-                    {loader: 'css-loader'}
-                ]
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist')
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.[chunkhash].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", 'index.html'),
-            filename: 'index.html'
-        })
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[chunkhash].js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }]
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, { loader: "css-loader" }]
+      }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.[chunkhash].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      filename: "index.html"
+    })
+  ]
 };
